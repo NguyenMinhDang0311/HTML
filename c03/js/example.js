22d34e81440bec967f4ad22a15e7547b6aec3abd
Nguyễn Minh Đăng <111495387+NguyenMinhDang0311@users.noreply.github.com>
2022-09-30 15:39:14 +0700

    c03

---
+++ c03/js/example.js
@@ -0,0 +1,41 @@
+(function (){
+    var hotel={
+        name:'Park',
+        roomRate:240,
+        discoumt:15,
+        offerPrice:function (){
+            var offerRate=this.roomRate*((100-this.discoumt)/100);
+            return offerRate;
+        }
+    };
+    var hotelName,roomRate,specialRate;
+
+    hotelName=document.getElementById('hotelName');
+    roomRate=document.getElementById('roomRate');
+    specialRate=document.getElementById('specialRate');
+
+    hotelName.textContent=hotel.name;
+    roomRate.textContent='$'+hotel.roomRate.toFixed(2);
+    specialRate.textContent='$'+hotel.offerPrice();
+
+    var expiryMsg;
+    var today;
+    var elEnds;
+
+    function offerExpires(today){
+        var weekFromToday,day,date,month,year,dayNames,monthName;
+        weekFromToday=new Date(today.getTime()+7*24*60*60*1000);
+        dayNames=['Sundat','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+        day=dayNames[weekFromToday.getDay()];
+        date=weekFromToday.getDate();
+        month=monthName[weekFromToday.getMonth()];
+        year=weekFromToday.getFullYear();
+
+        expiryMsg='Offer expires next';
+        expiryMsg += day+'<br/>('+date+''+month''+year+')';
+        return expiryMsg;
+    }
+    today=new Date();
+    elEnds=document.getElementById('odderEnds');
+    elEnds.innerHTML=offerExpires(today);
+}());
---
+++ c03/js/math-object.js
@@ -0,0 +1,3 @@
+var randomNum=math.floor((math.random() * 10) + 1);
+ var el=document.getElementById('info');
+ el.innerHTML='<h2>random number</h2><p>'+randomNum+'</p>';
